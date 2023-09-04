a52fcf0aff7a127962c898a66da7df18541ed34c
Bryan <bryanborck@gmail.com>
2023-09-04 01:07:51 -0300

    done

--- Lab02/client.cpp
+++ Lab02/client.cpp
@@ -1,8 +1,3 @@
-/* This page contains a client program that can request a file from the server program
-* on the next page. The server responds by sending the whole file.
-*/
-
-
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
@@ -11,34 +6,140 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#define SERVER_PORT 8080 /* arbitrary, but client & server must agree */
-#define BUFSIZE 4096  /* block transfer size */
+#include <chrono>
+#include <thread>
+#include <cstdio>
+#include <cmath>
+using namespace std::chrono_literals;
+
+#define SERVER_PORT 8080
+#define BUFSIZE 4096
+
+struct Drone {
+    int Drone_ID;
+    struct {
+        float x_coordinate;
+        float y_coordinate;
+        float z_coordinate;
+    } Drone_position;
+    struct {
+        float x_coordinate;
+        float y_coordinate;
+        float z_coordinate;
+    } Drone_speed;
+    struct {
+        float x_coordinate;
+        float y_coordinate;
+        float z_coordinate;
+    } Drone_target;
+};
+
 int main(int argc, char **argv)
 {
-  int c, s, bytes;
-  char buf[BUFSIZE];  /* buffer for incoming file */
-  struct hostent *h;  /* info about server */
-  struct sockaddr_in channel; /* holds IP address */
-  if (argc != 3) {printf("Usage: client server-name file-name"); exit(-1);}
-  h = gethostbyname(argv[1]); /* look up host’s IP address */
-  if (!h) {printf("gethostbyname failed to locate %s", argv[1]); exit(-1);}
-  
-  s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (s <0) {printf("socket call failed"); exit(-1);}
-  memset(&channel, 0, sizeof(channel));
-  channel.sin_family= AF_INET;
-  memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
-  channel.sin_port= htons(SERVER_PORT);
-  c = connect(s, (struct sockaddr *) &channel, sizeof(channel));
-  if (c < 0) {printf("connect failed"); exit(-1);} 
-  /* Connection is now established. Send file name including 0 byte at end. */
-  write(s, argv[2], strlen(argv[2])+1);
-  
-  /* Go get the file and write it to standard output.*/
-  while (1) {
-     bytes = read(s, buf, BUFSIZE); /* read from socket */
-     if (bytes <= 0) exit(0); /* check for end of file */
-     write(1, buf, bytes);  /* write to standard output */
-  }
-}
+    int s, bytes;
+    char buf[BUFSIZE]; 
+    struct hostent *h;
+    struct sockaddr_in channel;
+    struct Drone drone = {1, {100, 200, 50}, {0, 0, 0}, {0, 0, 0}};
 
+    if (argc != 2) {printf("Usage: client server-name"); exit(-1);}
+    
+    h = gethostbyname(argv[1]);
+    if (!h) {printf("gethostbyname failed to locate %s", argv[1]); exit(-1);}
+    
+    s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (s <0) {printf("socket call failed"); exit(-1);}
+    memset(&channel, 0, sizeof(channel));
+    channel.sin_family= AF_INET;
+    memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
+    channel.sin_port= htons(SERVER_PORT);
+
+    if (connect(s, (struct sockaddr *) &channel, sizeof(channel)) < 0) {
+        printf("connect failed");
+        exit(-1);
+    }
+
+    auto start_time = std::chrono::steady_clock::now();
+
+    while (1) {
+
+        auto current_time = std::chrono::steady_clock::now();
+
+        std::chrono::duration<double> elapsed_seconds = current_time - start_time;
+
+        drone.Drone_position.x_coordinate += drone.Drone_speed.x_coordinate * elapsed_seconds.count();
+        drone.Drone_position.y_coordinate += drone.Drone_speed.y_coordinate * elapsed_seconds.count();
+        drone.Drone_position.z_coordinate += drone.Drone_speed.z_coordinate * elapsed_seconds.count();
+
+        bytes = recv(s, buf, BUFSIZE, 0);
+        if (bytes < 0) {
+            printf("recv call failed");
+            exit(-1);
+        }
+        printf("Server Request: %s\n", buf);
+
+        if (strcmp(buf, "request_id") == 0) {
+            sprintf(buf, "%d", drone.Drone_ID);
+            send(s, buf, strlen(buf) + 1, 0);
+            bytes = recv(s, buf, BUFSIZE, 0);
+            if (bytes < 0) {
+                printf("recv call failed");
+                exit(-1);
+            }
+            printf("Target: (%s)\n", buf);
+            sscanf(buf, "%f, %f, %f", &drone.Drone_target.x_coordinate, &drone.Drone_target.y_coordinate, &drone.Drone_target.z_coordinate);
+
+            drone.Drone_speed.x_coordinate = (drone.Drone_target.x_coordinate - drone.Drone_position.x_coordinate);
+            drone.Drone_speed.y_coordinate = (drone.Drone_target.y_coordinate - drone.Drone_position.y_coordinate);
+            drone.Drone_speed.z_coordinate = (drone.Drone_target.z_coordinate - drone.Drone_position.z_coordinate);
+
+            float module = sqrt(pow(drone.Drone_speed.x_coordinate, 2) + pow(drone.Drone_speed.y_coordinate, 2) + pow(drone.Drone_speed.z_coordinate, 2));
+
+            drone.Drone_speed.x_coordinate = 2 * drone.Drone_speed.x_coordinate / module;
+            drone.Drone_speed.y_coordinate = 2 * drone.Drone_speed.y_coordinate / module;
+            drone.Drone_speed.z_coordinate = 2 * drone.Drone_speed.z_coordinate / module;
+        }
+        
+        if (strcmp(buf, "send_info") == 0) {
+            sprintf(buf, "%d: (%.0f, %.0f, %.0f), (%.1f, %.1f, %.1f)", drone.Drone_ID, 
+            drone.Drone_position.x_coordinate, drone.Drone_position.y_coordinate, drone.Drone_position.z_coordinate,
+            drone.Drone_speed.x_coordinate, drone.Drone_speed.y_coordinate, drone.Drone_speed.z_coordinate);
+            send(s, buf, strlen(buf) + 1, 0);
+        }
+        
+        if (strstr(buf, "send_move")) {
+            bytes = recv(s, buf, BUFSIZE, 0);
+            if (bytes < 0) {
+                printf("recv call failed");
+                exit(-1);
+            }
+            printf("NEW Target: (%s)\n", buf);
+            sscanf(buf, "%f, %f, %f", &drone.Drone_target.x_coordinate, &drone.Drone_target.y_coordinate, &drone.Drone_target.z_coordinate);
+
+            drone.Drone_speed.x_coordinate = (drone.Drone_target.x_coordinate - drone.Drone_position.x_coordinate);
+            drone.Drone_speed.y_coordinate = (drone.Drone_target.y_coordinate - drone.Drone_position.y_coordinate);
+            drone.Drone_speed.z_coordinate = (drone.Drone_target.z_coordinate - drone.Drone_position.z_coordinate);
+
+            int module = sqrt(pow(drone.Drone_speed.x_coordinate, 2) + pow(drone.Drone_speed.y_coordinate, 2) + pow(drone.Drone_speed.z_coordinate, 2));
+
+            drone.Drone_speed.x_coordinate = 2 * drone.Drone_speed.x_coordinate / module;
+            drone.Drone_speed.y_coordinate = 2 * drone.Drone_speed.y_coordinate / module;
+            drone.Drone_speed.z_coordinate = 2 * drone.Drone_speed.z_coordinate / module;
+
+            sprintf(buf, "ACK");
+            send(s, buf, strlen(buf) + 1, 0);
+        }
+
+        if (strstr(buf, "send_disconnect")) {
+            sprintf(buf, "ACK");
+            break;
+        }
+
+        start_time = current_time;
+    }
+
+    printf("Closing connection\n");
+
+    close(s);
+    return 0;
+}
--- Lab02/server.cpp
+++ Lab02/server.cpp
@@ -1,4 +1,3 @@
-/* This is the server code */
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
@@ -8,49 +7,147 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <chrono>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std::chrono_literals;
 
-#define SERVER_PORT 8080  /* arbitrary, but client & server must agree*/
-#define BUF_SIZE 4096  /* block transfer size */
+#define SERVER_PORT 8080
+#define BUF_SIZE 4096
 #define QUEUE_SIZE 10
 
-int main(int argc, char *argv[])
-{ 
-   int s, b, l, fd, sa, bytes, on = 1;
-   char buf[BUF_SIZE];  /* buffer for outgoing file */
-   struct sockaddr_in channel;  /* holds IP address */
-   /* Build address structure to bind to socket. */
-   memset(&channel, 0, sizeof(channel));
-   /* zero channel */
-   channel.sin_family = AF_INET;
-   channel.sin_addr.s_addr = htonl(INADDR_ANY);
-   channel.sin_port = htons(SERVER_PORT);
-   /* Passive open. Wait for connection. */
-   s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); /* create socket */
-   if (s < 0) {printf("socket call failed"); exit(-1);}
-   setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(on));
-   
-   b = bind(s, (struct sockaddr *) &channel, sizeof(channel));
-   if (b < 0) {printf("bind failed"); exit(-1);}
-   
-   l = listen(s, QUEUE_SIZE); /* specify queue size */
-   if (l < 0) {printf("listen failed"); exit(-1);}
-   /* Socket is now set up and bound. Wait for connection and process it. */
-   while (1) {
-      sa = accept(s, 0, 0); /* block for connection request */
-      if (sa < 0) {printf("accept failed"); exit(-1);}
+struct Target {
+    struct {
+        float x_coordinate;
+        float y_coordinate;
+        float z_coordinate;
+    } target;
+};
 
-      read(sa, buf, BUF_SIZE); /* read file name from socket */
+struct Drone {
+    int Drone_ID;
+    struct {
+        float x_coordinate;
+        float y_coordinate;
+        float z_coordinate;
+    } Drone_position;
+    struct {
+        float x_coordinate;
+        float y_coordinate;
+        float z_coordinate;
+    } Drone_speed;
+};
 
-      /* Get and return the file. */
-      fd = open(buf, O_RDONLY); /* open the file to be sent back */
-      if (fd < 0) printf("open failed");
-      while (1) {
-         bytes = read(fd, buf, BUF_SIZE); /* read from file */
-         if (bytes <= 0) break;  /* check for end of file */
-         write(sa, buf, bytes);  /* write bytes to socket */
-      }
-      close(fd); /* close file */
-      close(sa); /* close connection */
-   }   
+int main(int argc, char *argv[]) {
+    int s, b, l, sa, bytes, on = 1;
+    char buf[BUF_SIZE];
+    struct sockaddr_in channel;
+    struct Target target = {250, 300, 50};
+    struct Drone drone = {0, {0, 0, 0}, {0, 0, 0}};
+
+    std::vector<std::string> authorized_ids = {"1", "2", "31"};
+
+    memset(&channel, 0, sizeof(channel));
+    channel.sin_family = AF_INET;
+    channel.sin_addr.s_addr = htonl(INADDR_ANY);
+    channel.sin_port = htons(SERVER_PORT);
+
+    s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (s < 0) {printf("socket call failed"); exit(-1);}
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+    b = bind(s, (struct sockaddr *) &channel, sizeof(channel));
+    if (b < 0) {printf("bind failed"); exit(-1);}
+
+    l = listen(s, QUEUE_SIZE);
+    if (l < 0) {printf("listen failed"); exit(-1);}
+
+    printf("Server is ready\n");
+
+    while (true) {
+        sa = accept(s, 0, 0);
+        if (sa < 0) {
+            printf("accept failed");
+            exit(-1);
+        }
+
+        strcpy(buf, "request_id");
+        send(sa, buf, strlen(buf) + 1, 0);
+        bytes = recv(sa, buf, BUF_SIZE, 0);
+        if (bytes <= 0) { close(sa); continue; }
+
+        bool found = false;
+        for (const std::string& str : authorized_ids) {
+            if (strcmp(str.c_str(), buf) == 0) {
+                found = true;
+                break;
+            }
+        }
+        if (bytes <= 0 || found == false) {
+            printf("Unauthorized connection or connection error. Disconnecting client.\n");
+            strcpy(buf, "send_disconnect");
+            send(sa, buf, strlen(buf) + 1, 0);
+            close(sa);
+            continue;
+        }
+        printf("Authorized drone connected: ID = %s\n", buf);
+        sprintf(buf, "%.0f, %.0f, %.0f", target.target.x_coordinate, target.target.y_coordinate, target.target.z_coordinate);
+        send(sa, buf, strlen(buf) + 1, 0);
+
+        auto last_info_request_time = std::chrono::steady_clock::now();
+        auto last_sent_time = std::chrono::steady_clock::now();
+        bool done = true;
+
+        while (true) {
+            auto current_time = std::chrono::steady_clock::now();
+            if (std::chrono::duration_cast<std::chrono::seconds>(current_time - last_info_request_time) >= 5s) {
+                printf("Sending info instruction to drone\n");
+                strcpy(buf, "send_info");
+                send(sa, buf, strlen(buf) + 1, 0);
+
+                bytes = recv(sa, buf, BUF_SIZE, 0);
+                if (bytes <= 0) { close(sa); continue; }
+                printf("Drone Response: %s\n", buf);
+
+                sscanf(buf, "%d: (%f, %f, %f), (%f, %f, %f)", &drone.Drone_ID, 
+                &drone.Drone_position.x_coordinate, &drone.Drone_position.y_coordinate, &drone.Drone_position.z_coordinate,
+                &drone.Drone_speed.x_coordinate, &drone.Drone_speed.y_coordinate, &drone.Drone_speed.z_coordinate);
+
+                last_info_request_time = current_time;
+            }
+
+            if (std::chrono::duration_cast<std::chrono::seconds>(current_time - last_sent_time) >= 10s && done) {
+                printf("Sending target change instruction to drone\n");
+                strcpy(buf, "send_move");
+                send(sa, buf, strlen(buf) + 1, 0);
+
+                target = {300, 100, 60};
+                sprintf(buf, "%.0f, %.0f, %.0f", target.target.x_coordinate, target.target.y_coordinate, target.target.z_coordinate);
+                send(sa, buf, strlen(buf) + 1, 0);
+
+                bytes = recv(sa, buf, BUF_SIZE, 0);
+                if (bytes <= 0) { close(sa); continue; }
+                printf("Drone Response: %s\n", buf);
+
+                last_sent_time = current_time;
+                done = false;
+            }
+
+            if ((target.target.x_coordinate - 10) < drone.Drone_position.x_coordinate && drone.Drone_position.x_coordinate < (target.target.x_coordinate + 10) &&
+                (target.target.y_coordinate - 10) < drone.Drone_position.y_coordinate && drone.Drone_position.y_coordinate < (target.target.y_coordinate + 10) &&
+                (target.target.z_coordinate - 10) < drone.Drone_position.z_coordinate && drone.Drone_position.z_coordinate < (target.target.z_coordinate + 10))
+            {
+                printf("Target achieved.\n");
+                strcpy(buf, "send_disconnect");
+                send(sa, buf, strlen(buf) + 1, 0);
+                close(sa);
+                close(s);
+                break;
+            }
+        }
+    }
+
+    close(s);
+    return 0;
 }
-
